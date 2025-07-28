a62cd87964daaff4b2926abb967e8dff3d789629
Saad-d <saadaltafkhan456@gmail.com>
2025-07-28 16:56:36 +0530

    Feat:Writen a logic for Age Calcultaion

--- nikha/nikha/doctype/marriage_certificate/marriage_certificate.py
+++ nikha/nikha/doctype/marriage_certificate/marriage_certificate.py
@@ -3,7 +3,30 @@
 
 # import frappe
 from frappe.model.document import Document
-
+from datetime import date
+from frappe.utils import getdate
 
 class MarriageCertificate(Document):
-	pass
+	def before_save(self):
+		if self.dob:
+			dob_date = getdate(self.dob)
+			today = date.today()
+			age = today.year - dob_date.year - (
+				(today.month, today.day) < (dob_date.month, dob_date.day)
+			)
+			self.groom_age = str(age)
+
+		if self.bride_dob:
+			dob_date = getdate(self.bride_dob)
+			today = date.today()
+			age_of_bride = today.year - dob_date.year - (
+				(today.month, today.day) < (dob_date.month, dob_date.day)
+			)
+			self.age = str(age_of_bride)
+	# def before_save(self):
+    #     if self.dob:
+    #         today = date.today()
+    #         self.groom_age = today.year - self.dob.year - (
+    #             (today.month, today.day) < (self.dob.month, self.dob.day)
+    #         )
+	
